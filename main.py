a761d93eec6c0fea2e108901ffb7e8047500756a
colewortman <wortcole12@gmail.com>
2024-06-06 20:42:05 -0500

    added letter_count function

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        words = file_contents.split()
+
+        print(len(words))
+        print(letter_count(words))
+
+def letter_count(words):
+    letter_count = {}
+
+    for word in words:
+        lowered_word = word.lower()
+        for character in lowered_word:
+            if character in letter_count:
+                letter_count[character] += 1
+            else:
+                letter_count[character] = 1
+    
+    return letter_count
+
 main()
